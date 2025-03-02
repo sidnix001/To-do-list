f8bc54a161cf93e616bcdb924ed2e201b376d214
Sidnix <vmakisev65@gmail.com>
2025-03-02 12:35:57 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
    $("#addBtn").on("click", function () {
-      $("#notes__container").append(`<p class="note">${$("#note__text").val()}</p>`)
+      if ($("#note__text").val() != "/fz2") {
+         $("#notes__container").append(`<p class="note">${$("#note__text").val()}</p>`)
+      } else {
+         $("*").css("font-size", "1.2rem")
+      }
       $(".note").click(function () {
          $(this).css("transform", "scale(0)");
          setTimeout(() => {$(this).remove()}, 100);
