94c30fb0355e83bbe0d65512d32fd07da9b4450c
Sidnix <vmakisev65@gmail.com>
2025-04-07 16:33:41 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
       if ($("#note__text").val() !== "/font-size") {
          $("#notes__container").append(`<p class="note">${$("#note__text").val()}</p>`)
       } else {
-         let userFontSize = prompt("Font size (default: 1)")
+         let userFontSize = prompt("Font size (1 = 16px | default: 1.3)")
          $("*").css("font-size", `${userFontSize}rem`)
       } 
       $(".note").click(function () {
